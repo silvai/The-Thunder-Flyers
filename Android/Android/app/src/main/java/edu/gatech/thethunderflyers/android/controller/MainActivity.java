e971b1e752451eb40d0b04598a300b10163dbab5
pace543 <sribhat31@gmail.com>
2017-10-05 17:53:06 -0400

    Updated Adapter and XML layout

--- Android/Android/app/src/main/java/edu/gatech/thethunderflyers/android/controller/MainActivity.java
+++ Android/Android/app/src/main/java/edu/gatech/thethunderflyers/android/controller/MainActivity.java
@@ -3,6 +3,8 @@ package edu.gatech.thethunderflyers.android.controller;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import edu.gatech.thethunderflyers.android.R;
@@ -10,6 +12,10 @@ import edu.gatech.thethunderflyers.android.model.RatData;
 import edu.gatech.thethunderflyers.android.util.AsyncHandler;
 
 public class MainActivity extends AppCompatActivity implements AsyncHandler<RatData[]> {
+
+    private RecyclerView dataView;
+    private RatDataAdapter dataAdapter;
+    private LinearLayoutManager dataManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
