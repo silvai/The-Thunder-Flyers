9ecf543a9162d08d6ad0e77f2bdc6fe65f34148a
Steven Nguyen <stevennguyen430@gmail.com>
2017-10-05 17:01:08 -0400

    modified rat data, moved to model

---
+++ Android/Android/app/src/main/java/edu/gatech/thethunderflyers/android/model/RatData.java
@@ -0,0 +1,18 @@
+package edu.gatech.thethunderflyers.android.model;
+import java.util.Date;
+
+public class RatData {
+    /*
+Location Type (make an ENUM)
+Borough (ENUM)
+     */
+    private int id;
+    private Date date;
+
+    private int zip;
+    private int address;
+    private String city;
+
+    private double latitude;
+    private double longitude;
+}
