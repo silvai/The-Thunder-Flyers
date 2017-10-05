4561db04d9f62487ed4bd24be231e44dca686744
pace543 <sribhat31@gmail.com>
2017-10-05 17:37:01 -0400

    Added RecyclerView XML and adapter

---
+++ Android/Android/app/src/main/java/edu/gatech/thethunderflyers/android/controller/RatDataAdapter.java
@@ -0,0 +1,21 @@
+package edu.gatech.thethunderflyers.android.controller;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class RatDataAdapter extends RecyclerView.Adapter{
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
