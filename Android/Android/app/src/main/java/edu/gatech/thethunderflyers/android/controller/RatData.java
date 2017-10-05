1dada55a64a9852481d4e7032f8e673724630858
Steven Nguyen <stevennguyen430@gmail.com>
2017-10-05 16:57:07 -0400

    add RatData class

---
+++ Android/Android/app/src/main/java/edu/gatech/thethunderflyers/android/controller/RatData.java
@@ -0,0 +1,18 @@
+package edu.gatech.thethunderflyers.android.controller;
+import java.util.Date;
+
+public class RatData {
+    /*
+Unique Key
+Created Date
+Location Type
+Incident Zip
+Incident Address
+City
+Borough
+Latitude
+Longitude
+     */
+    private int id;
+    private Date date;
+}
