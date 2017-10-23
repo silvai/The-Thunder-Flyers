44a712eaa9f5cc696a09ade83cae0a51099c4a5b
Isabella Silva <isabellasilva@lawn-143-215-125-22.lawn.gatech.edu>
2017-10-23 17:12:47 -0400

    loading activity work in progress

---
+++ Android/Android/app/src/main/java/edu/gatech/thethunderflyers/android/controller/LoadingActivity.java
@@ -0,0 +1,15 @@
+package edu.gatech.thethunderflyers.android.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.thethunderflyers.android.R;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+}
