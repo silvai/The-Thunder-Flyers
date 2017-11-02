5d4ba4177914b70f2b338531486af42ef470b452
Steven Nguyen <stevennguyen430@gmail.com>
2017-11-02 16:51:20 -0400

    android chart stuff

--- Android/Android/app/src/main/java/edu/gatech/thethunderflyers/android/controller/GraphActivity.java
+++ Android/Android/app/src/main/java/edu/gatech/thethunderflyers/android/controller/GraphActivity.java
@@ -8,16 +8,16 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Toast;
 
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
-import com.jjoe64.graphview.series.Series;
 
 import java.lang.ref.WeakReference;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -30,10 +30,11 @@ import edu.gatech.thethunderflyers.android.util.AsyncHandler;
 import edu.gatech.thethunderflyers.android.util.Navigator;
 
 public class GraphActivity extends AppCompatActivity implements AsyncHandler<List<RatData>> {
-    private GraphView graph;
     private Button begin;
     private Button end;
     private Button submit;
+    private GraphView graph;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +43,7 @@ public class GraphActivity extends AppCompatActivity implements AsyncHandler<Lis
         submit = (Button) findViewById(R.id.submitDates);
         begin = (Button) findViewById(R.id.beginDate);
         end = (Button) findViewById(R.id.endDate);
+
         begin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -97,7 +99,15 @@ public class GraphActivity extends AppCompatActivity implements AsyncHandler<Lis
         SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
         try {
             Date dateBegin = format.parse((String) begin.getText());
+            Calendar c1 = Calendar.getInstance();
+            c1.setTime(dateBegin);
+            c1.set(Calendar.DATE, c1.getActualMinimum(Calendar.DATE));
+            dateBegin = c1.getTime();
             Date dateEnd = format.parse((String) end.getText());
+            Calendar c2= Calendar.getInstance();
+            c2.setTime(dateEnd);
+            c2.set(Calendar.DATE, c2.getActualMaximum(Calendar.DATE));
+            dateEnd = c2.getTime();
             if (dateBegin.compareTo(dateEnd) > 0 || dateBegin == null ||dateEnd == null) {
                 Toast.makeText(this, "Dates invalid!", Toast.LENGTH_SHORT).show();
             } else {
@@ -118,17 +128,75 @@ public class GraphActivity extends AppCompatActivity implements AsyncHandler<Lis
 
     @Override
     public void handleResponse(List<RatData> response, Exception ex) {
-        //list of dates
-        //put into series
-        //for the same mm/dd/yyyy then increment x value
+        graph.removeAllSeries();
+        final Calendar d1 = Calendar.getInstance();
+        final Calendar d2 = Calendar.getInstance();
 
-//        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-//          new DataPoint(0, 1),
-//          new DataPoint(1, 5),
-//          new DataPoint(2, 3),
-//          new DataPoint(3, 2),
-//          new DataPoint(4, 6)
-//        });
-//        graph.addSeries(series);
+        final Calendar iter = Calendar.getInstance();
+
+        if (response.size() == 0) {
+            return;
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        Date beginDate = response.get(0).getDate();
+        Date endDate = response.get(response.size() - 1).getDate();
+        try {
+            endDate = sdf.parse((String) end.getText());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        d1.setTime(beginDate);
+        d2.setTime(endDate);
+        d2.set(Calendar.DATE, d2.getActualMaximum(Calendar.DATE));
+
+        int months = getMonthDiff(d1, d2) + 1;
+
+        final Date[] myears = new Date[months];
+
+        iter.setTime(beginDate);
+        iter.set(Calendar.DATE, iter.getActualMaximum(Calendar.DATE) / 2);
+
+        for (int j = 0; j < months; j++) {
+            myears[j] = iter.getTime();
+            iter.add(Calendar.MONTH, 1);
+        }
+
+        int[] yVals = new int[months];
+        for (RatData rd: response) {
+            Calendar dateCal = Calendar.getInstance();
+            Date date = rd.getDate();
+            dateCal.setTime(date);
+            int arrayLocation = getMonthDiff(dateCal, d2);
+            yVals[months - arrayLocation - 1] += 1;
+        }
+        System.out.println(Arrays.toString(myears));
+        System.out.println(Arrays.toString(yVals));
+
+        BarGraphSeries barGraph = new BarGraphSeries();
+        for (int i = 0; i < yVals.length; i++) {
+            barGraph.appendData(new DataPoint(myears[i], yVals[i]), true, 100);
+        }
+
+        barGraph.setSpacing(15);
+
+        graph.addSeries(barGraph);
+        graph.getGridLabelRenderer().setNumHorizontalLabels(3);
+        graph.getViewport().setMinX(myears[0].getTime());
+        graph.getViewport().setMaxX(myears[myears.length - 1].getTime());
+
+        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this, new SimpleDateFormat("MM/yyyy")));
+
+        graph.getViewport().setXAxisBoundsManual(true);
+
+        graph.getGridLabelRenderer().setHumanRounding(false);
+
+        graph.setTitle("Reports per Month");
+    }
+
+    public int getMonthDiff(Calendar d1, Calendar d2){
+        int diff = (d2.get(Calendar.YEAR) - d1.get(Calendar.YEAR)) * 12 + d2.get(Calendar.MONTH) - d1.get(Calendar.MONTH);
+        return diff;
     }
 }
