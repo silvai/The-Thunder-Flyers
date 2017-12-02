00f93dc492a292ec1f3264429253ca55ee99b875
Steven Nguyen <stevennguyen430@gmail.com>
2017-12-02 12:29:50 -0500

    added some stuff to graph

--- javafxRatApp/ratapp/src/main/java/edu/gatech/thethunderflyers/ratapp/controller/GraphActivity.java
+++ javafxRatApp/ratapp/src/main/java/edu/gatech/thethunderflyers/ratapp/controller/GraphActivity.java
@@ -1,4 +1,46 @@
 package edu.gatech.thethunderflyers.ratapp.controller;
 
-public class GraphActivity {
+import edu.gatech.thethunderflyers.ratapp.model.Graphs;
+import edu.gatech.thethunderflyers.ratapp.util.Navigator;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.Chart;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GraphActivity implements Initializable{
+    private Chart chart;
+    private Navigator navigator = new Navigator();
+    @FXML
+    private ChoiceBox<Graphs> graphBox;
+    @FXML
+    private Button mainMenuButton;
+
+    public void graph() {
+
+        Graphs selected = graphBox.getSelectionModel().getSelectedItem();
+        switch (selected) {
+//            case BAR_CHART: chart = new BarChart<>();
+//            break;
+//            case PIE_CHART: chart = new PieChart();
+//            break;
+//            case LINE_CHART: chart = new LineChart<>();
+//            break;
+        }
+    }
+
+    public void mainMenu() {
+        navigator.navigate("res/mainMenuView.fxml", "Main Menu",
+                (Stage) mainMenuButton.getScene().getWindow());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        graphBox.getItems().setAll(Graphs.values());
+        graphBox.getSelectionModel().selectFirst();
+    }
 }
--- javafxRatApp/ratapp/src/main/java/edu/gatech/thethunderflyers/ratapp/controller/ReportActivity.java
+++ javafxRatApp/ratapp/src/main/java/edu/gatech/thethunderflyers/ratapp/controller/ReportActivity.java
@@ -1,4 +1,89 @@
 package edu.gatech.thethunderflyers.ratapp.controller;
 
-public class ReportActivity {
+import edu.gatech.thethunderflyers.ratapp.model.*;
+import edu.gatech.thethunderflyers.ratapp.util.APIClient;
+import edu.gatech.thethunderflyers.ratapp.util.AsyncHandler;
+import edu.gatech.thethunderflyers.ratapp.util.Navigator;
+import edu.gatech.thethunderflyers.ratapp.util.Validator;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import sun.rmi.runtime.Log;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReportActivity implements Initializable, AsyncHandler<APIMessage> {
+    private Navigator navigator = new Navigator();
+    private Validator validate;
+    private Alert alert;
+    private static APIMessage apiMessage;
+
+    @FXML
+    private Button submitButton, cancelButton;
+    @FXML
+    private TextField addressText, cityText, zipText, latitudeText, longitudeText;
+    @FXML
+    private ChoiceBox<Borough> boroughBox;
+    @FXML
+    private ChoiceBox<LocationType> locationTypeBox;
+
+    public void submit() {
+        validate = new Validator(addressText, cityText, zipText, latitudeText, longitudeText);
+        if (validate.validate()) {
+            int zip = Integer.parseInt(zipText.getText());
+            double lat = Double.parseDouble(latitudeText.getText());
+            double longitude = Double.parseDouble(longitudeText.getText());
+
+            //need to change user id?
+            //need to get the correct token.....
+
+            APIClient.API_CLIENT.submitRatReport(
+                    Model.getRatData(locationTypeBox.getSelectionModel().getSelectedItem(),
+                            zip, addressText.getText(), cityText.getText(),
+                            boroughBox.getSelectionModel().getSelectedItem(), lat,
+                            longitude, 11), apiMessage.getMessage(), this);
+        } else {
+            alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error!");
+            alert.setHeaderText("One or more fields are empty!");
+            alert.show();
+        }
+    }
+
+    public void cancel() {
+        navigator.navigate("res/mainMenuView.fxml", "Main Menu",
+                (Stage) cancelButton.getScene().getWindow());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        boroughBox.getItems().setAll(Borough.values());
+        boroughBox.getSelectionModel().selectFirst();
+
+        locationTypeBox.getItems().setAll(LocationType.values());
+        locationTypeBox.getSelectionModel().selectFirst();
+    }
+
+    @Override
+    public void handleResponse(APIMessage response) {
+        apiMessage = response;
+        if (response.getSuccess()) {
+            navigator.navigate("mainMenuView.fxml", "Main Menu",
+                    (Stage) submitButton.getScene().getWindow());
+            alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success!");
+            alert.setHeaderText("New Report Entered");
+            alert.show();
+        } else {
+            alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error!");
+            alert.setHeaderText("One or more fields are invalid!");
+            alert.show();
+        }
+    }
 }
